3390ec89b8fc02cd91b7412f7ab34f8744b1b397
zopaj63 <zoran.pajnic@gmail.com>
2023-09-24 15:22:22 +0200

    unos iz forme#

--- app/Http/Controllers/NumberController.php
+++ app/Http/Controllers/NumberController.php
@@ -6,14 +6,15 @@ use Illuminate\Http\Request;
 
 class NumberController extends Controller
 {
+    /*
     public function add($a, $b)
     {
         $sum=$a+$b;
         //vrati u pogled sum vrijednost sum
         return view("sum", ["sum"=>$sum]); //pogled nazvan sum, plus ključ i vrijednost asocijativnog niza
     }
-    
-/*
+    */
+
     public function add(Request $request)
     {
         $broj1=$request->input("broj1");
@@ -23,7 +24,7 @@ class NumberController extends Controller
 
         return view("sum", ["sum"=>$sum]); //sum.blade.php u view
     }
-    */
+    
 
 }
 
